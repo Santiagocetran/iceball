c4c5286ad3b34afe85bf94ed54a4a8a148303600
santiagocetran <santiagorcetran236@gmail.com>
2025-08-29 01:05:11 -0300

    Fix build issues: remove GLEW dependency and revert sackit memory loading
    - Remove -lGLEW from Makefile (not available on system)
    - Revert sackit_module_load_memory to temporary file approach for compatibility
    - These changes allow successful compilation on systems without GLEW and with older sackit versions
    
    Build tested successfully on Linux with:
    - SDL2, ENet, LuaJIT, zlib, OpenGL
    - sackit library built from source

--- src/lua_fetch.h
+++ src/lua_fetch.h
@@ -354,7 +354,25 @@ int icelua_fn_common_fetch_poll(lua_State *L)
 				lua_pushlstring(L, to_client_local.cfetch_ubuf, to_client_local.cfetch_ulen);
 				ret = 1;
 #else
-				it_module_t *mus = sackit_module_load_memory(to_client_local.cfetch_ubuf, to_client_local.cfetch_ulen);
+				// create temp file (sackit doesn't support loading from memory, at least right now)
+				// "Never use this function." i have no other choice
+				char *tfname = tempnam(NULL, "ibsit");
+				if(tfname == NULL)
+				{
+					ret = 0;
+					break;
+				}
+				FILE *fp = fopen(tfname, "wb");
+				if(fp == NULL)
+				{
+					ret = 0;
+					free(tfname);
+					break;
+				}
+				fwrite(to_client_local.cfetch_ubuf, to_client_local.cfetch_ulen, 1, fp);
+				fclose(fp);
+
+				it_module_t *mus = sackit_module_load(tfname);
 				if(mus == NULL)
 				{
 					ret = 0;
@@ -362,6 +380,8 @@ int icelua_fn_common_fetch_poll(lua_State *L)
 					ret = 1;
 					lua_pushlightuserdata(L, mus);
 				}
+
+				free(tfname);
 #endif
 			} break;
 
